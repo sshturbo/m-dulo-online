0b976540d66b8cea0e38da9d970598f5b40ec236
Jefferson Hipolito <149891602+sshturbo@users.noreply.github.com>
2024-04-13 11:13:54 -0300

    Add files via upload

--- online.go
+++ online.go
@@ -1,102 +1,98 @@
 package main
 
 import (
-    "bufio"
-    "bytes"
-    "fmt"
-    "net/http"
-    "os"
-    "os/exec"
-    "strings"
-    "time"
+	"bufio"
+	"bytes"
+	"fmt"
+	"net/http"
+	"os"
+	"os/exec"
+	"strings"
+	"time"
 )
 
 func main() {
-    if len(os.Args) != 2 {
-        fmt.Println("Uso: programa <URL>")
-        return
-    }
-    url := os.Args[1]
-    startLoop(url)
+	if len(os.Args) != 2 {
+		fmt.Println("Uso: ./programa <URL>")
+		return
+	}
+	url := os.Args[1]
+	startLoop(url)
 }
 
 func startLoop(url string) {
-    for {
-        userList, err := getUsers()
-        if err != nil {
-            fmt.Println("Erro ao obter a lista de usuários:", err)
-            time.Sleep(3 * time.Second)
-            continue
-        }
+	for {
+		userList, err := getUsers()
+		if err != nil {
+			fmt.Println("Erro ao obter a lista de usuários:", err)
+			time.Sleep(3 * time.Second)
+			continue
+		}
 
-        if err := sendPostRequest(url, userList); err != nil {
-            fmt.Println("Erro ao enviar POST request:", err)
-        }
+		if err := sendPostRequest(url, userList); err != nil {
+			fmt.Println("Erro ao enviar POST request:", err)
+		}
 
-        time.Sleep(3 * time.Second)
-    }
+		time.Sleep(3 * time.Second)
+	}
 }
 
 func getUsers() (string, error) {
-    var userList []string
+	var userList []string
 
-    output, err := exec.Command("sh", "-c", "ps aux | grep priv | grep Ss").Output()
-    if err != nil {
-        return "", err
-    }
+	// Obtém usuários do sistema
+	output, err := exec.Command("sh", "-c", "ps aux | grep priv | grep Ss").Output()
+	if err != nil {
+		return "", err
+	}
+	scanner := bufio.NewScanner(strings.NewReader(string(output)))
+	for scanner.Scan() {
+		line := scanner.Text()
+		columns := strings.Fields(line)
+		if len(columns) >= 12 {
+			// Encontra o índice do padrão "sshd:"
+			index := -1
+			for i, col := range columns {
+				if col == "sshd:" {
+					index = i
+					break
+				}
+			}
+			// Se o padrão "sshd:" for encontrado, adicione o nome de usuário à lista
+			if index != -1 && index+1 < len(columns) {
+				username := columns[index+1]
+				userList = append(userList, username)
+			}
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		return "", err
+	}
 
-    scanner := bufio.NewScanner(strings.NewReader(string(output)))
-    for scanner.Scan() {
-        line := scanner.Text()
-        if !strings.Contains(line, "priv") {
-            continue
-        }
-        columns := strings.Fields(line)
-        if len(columns) >= 12 {
-            username := columns[11] // Alterado para pegar o usuário da coluna 10
-            // Adicione apenas o nome de usuário à lista, ignorando outros caracteres
-            username = strings.TrimSpace(username)
-            if !strings.Contains(username, "-c") { // Verifica se o usuário contém "-c"
-                userList = append(userList, username)
-            }
-        }
-    }
-    if err := scanner.Err(); err != nil {
-        return "", err
-    }
+	// Se o arquivo openvpn-status.log existir, adicione os usuários do OpenVPN
+	if _, err := os.Stat("/etc/openvpn/openvpn-status.log"); err == nil {
+		openVPNOutput, err := exec.Command("sh", "-c", "cat /etc/openvpn/openvpn-status.log | grep -Eo '^[a-zA-Z0-9_-]+,[0-9]+\\.[0-9]+\\.[0-9]+\\.[0-9]+:[0-9]+' | awk -F, '{print $1}'").Output()
+		if err != nil {
+			return "", err
+		}
+		openVPNUsers := strings.Split(strings.TrimSpace(string(openVPNOutput)), "\n")
+		for _, user := range openVPNUsers {
+			userList = append(userList, user)
+		}
+	}
 
-    // Verifica se o arquivo do OpenVPN existe antes de tentar recuperar os usuários
-    if _, err := exec.LookPath("grep"); err == nil {
-        if _, err := exec.Command("grep", "-q", "^[a-zA-Z0-9_-]+,[0-9]+\\.[0-9]+\\.[0-9]+\\.[0-9]+:[0-9]+", "/etc/openvpn/openvpn-status.log").Output(); err == nil {
-            openVPNOutput, err := exec.Command("grep", "-Eo", "^[a-zA-Z0-9_-]+,[0-9]+\\.[0-9]+\\.[0-9]+\\.[0-9]+:[0-9]+", "/etc/openvpn/openvpn-status.log").Output()
-            if err != nil {
-                return "", err
-            }
-            openVPNUsers := strings.Split(strings.TrimSpace(string(openVPNOutput)), "\n")
-            for _, user := range openVPNUsers {
-                // Adicione apenas o nome de usuário à lista, ignorando outros caracteres
-                user = strings.TrimSpace(user)
-                if !strings.Contains(user, "-c") { // Verifica se o usuário contém "-c"
-                    userList = append(userList, user)
-                }
-            }
-        }
-    }
-
-    return strings.Join(userList, ","), nil
+	return strings.Join(userList, ","), nil
 }
 
 func sendPostRequest(url, userList string) error {
-    // Crie um buffer com os dados a serem enviados no corpo da requisição
-    requestBody := []byte("users=" + userList)
-    body := bytes.NewBuffer(requestBody)
+	// Fazer uma requisição POST para a URL especificada
+	requestBody := []byte("users=" + userList)
+	resp, err := http.Post(url, "application/x-www-form-urlencoded", bytes.NewBuffer(requestBody))
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
 
-    // Faça uma requisição POST para a URL especificada com os dados do corpo
-    _, err := http.Post(url, "application/x-www-form-urlencoded", body)
-    if err != nil {
-        return err
-    }
-
-    fmt.Printf("Enviando lista de usuários %s para %s\n", userList, url)
-    return nil
+	fmt.Printf("Enviando lista de usuários %s para %s\n", userList, url)
+	return nil
 }
